199c9219ec19d8f300b583e85cedbad674915077
Khodepanda <khodepanda@gmail.com>
2023-02-19 22:19:03 +0100

    Update file

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Beginning of task
         </a>
       </header>
     </div>
